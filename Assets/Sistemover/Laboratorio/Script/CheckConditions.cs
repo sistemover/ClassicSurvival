9276bbd172cf90adbd1e4d2d3c30f9a50dd14124
sistemover <31515147+sistemover@users.noreply.github.com>
2018-03-19 21:51:57 -0300

    Desarrollo - Índice de Condiciones en Debug.
    Se crea un script que permite observar mediante la Consola el estado de las condiciones definidas en el Juego, notese que son de Tipo ScriptableObject, pero es importante llevar registros persistentes de tipo SGame para gestionar futuros tipos de partidas... supongo.
    
    Se Crea botón que resetea directamente las condiciones.

---
+++ Assets/Sistemover/Laboratorio/Script/CheckConditions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckConditions : MonoBehaviour {
+
+	public AllConditions condiciones;
+
+	void Update ()
+	{
+		for (int i = 0; i < condiciones.conditions.Length; i++) {
+			Debug.Log (condiciones.conditions [i].description + " " +condiciones.conditions[i].satisfied);
+		}
+	}
+
+	public void ResetearCondiciones()
+	{
+		condiciones.Reset ();
+	}
+}
